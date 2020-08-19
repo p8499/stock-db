f607fa478a0d6015d6d7e12a0295e2eb0744c276
p8499 <p8499@126.com>
2020-08-19 09:47:44 +0800

    Add files via upload

--- tables/cache.sql
+++ tables/cache.sql
@@ -1,6 +1,9 @@
 DROP TABLE IF EXISTS finance;
-CREATE TABLE finance (
-  symbol CHAR(11) NOT NULL,
-  date   DATE     NOT NULL,
-  PRIMARY KEY (symbol, date)
+CREATE TABLE cache(
+  symbol CHAR(11)    NOT NULL,
+  date   DATE        NOT NULL,
+  kpi    VARCHAR(16) NOT NULL,
+  val    NUMERIC(32, 8),
+  PRIMARY KEY (symbol, date, kpi)
 );
+
